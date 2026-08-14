a63ee6480c00da90808659e9fa886d8d161c6f81
Tomas Laurenzo <tomas@laurenzo.net>
2026-08-14 01:23:30 -0300

    Stage History Through CU v3 loader

---
+++ deployments/history-through-cu/data-v3.js
@@ -0,0 +1,9 @@
+(() => {
+  document.write('<link rel="stylesheet" href="v3-refinement.css">');
+  document.write('<link rel="stylesheet" href="trove-intros-early.css">');
+  document.write('<link rel="stylesheet" href="trove-intros-late.css">');
+  document.write('<link rel="stylesheet" href="v3-mobile-polish.css">');
+  document.write('<link rel="stylesheet" href="trove-intro-final-polish.css">');
+  document.write('<script src="data-v3-core.js"><\/script>');
+  document.write('<script src="trove-refinement.js"><\/script>');
+})();
